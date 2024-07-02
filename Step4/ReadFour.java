4986bef380a873c88a55b6e3158c986562a8a63b
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-07-02 15:55:11 -0400

    Done.

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -12,7 +12,24 @@ public class ReadFour
      */
         public static void main(String[] args)
         {
-            // Print out a running total of all the
-            // values in the input file.
+            try {
+                Scanner fileIn = new Scanner(new File("/Users/qian/Desktop/ZipCode/Java/FileIOJava/Step4/input.txt"));
+                int sum =0;
+                while(fileIn.hasNext()){
+                    String lineIn = fileIn.nextLine();
+
+                    String[] input = lineIn.split("\n");
+                    //int sum=0;
+                    for(int i=0;i<input.length;i++){
+                        sum += Integer.parseInt(input[i]);
+                        System.out.println("running total is " +sum+"\n");
+                    }
+
+
+                }
+
+            }catch (IOException e){
+                System.out.println("File not found.");
+            }
         }
 }
